6f1cebd3dfba69f7e83711ca27061a088af6d29b
Joachim Kret <joachim.kret@siroccomobile.com>
2023-05-25 12:12:41 +0200

    Add swift format tool as pre-commit hook.

--- UIGallery/UIGallery/Utilities/Combine/LoadingPublisher.swift
+++ UIGallery/UIGallery/Utilities/Combine/LoadingPublisher.swift
@@ -4,16 +4,16 @@ import Foundation
 class LoadingPublisher<Action, Value, Error: Swift.Error>: Publisher {
     typealias Failure = Never
     typealias Output = LoadingState<Value, Error>
-    
+
     private let scheduler: AnySchedulerOf<DispatchQueue>
     private let currentState = CurrentValueSubject<Output, Failure>(.initial)
     private let createPublisher: (Action) -> AnyPublisher<Value, Error>
     private var cancellable: Cancellable?
-    
+
     var state: Output {
-        return currentState.value
+        currentState.value
     }
-    
+
     init(
         scheduler: AnySchedulerOf<DispatchQueue> = .main,
         createPublisher: @escaping (Action) -> AnyPublisher<Value, Error>
@@ -21,15 +21,15 @@ class LoadingPublisher<Action, Value, Error: Swift.Error>: Publisher {
         self.scheduler = scheduler
         self.createPublisher = createPublisher
     }
-    
+
     func send(_ action: Action, force: Bool = false) {
         guard state.isLoading || force else {
             return
         }
-        
+
         let loading = Just(Output.loading)
             .eraseToAnyPublisher()
-        
+
         let completion = createPublisher(action)
             .first()
             .map {
@@ -41,17 +41,18 @@ class LoadingPublisher<Action, Value, Error: Swift.Error>: Publisher {
                     .eraseToAnyPublisher()
             }
             .eraseToAnyPublisher()
-        
+
         cancellable = Publishers.Concatenate(
             prefix: loading,
-            suffix: completion)
+            suffix: completion
+        )
         .receive(on: scheduler)
         .sink(receiveValue: { [currentState] in
             currentState.value = $0
         })
     }
-    
-    func receive<S>(subscriber: S) where S : Subscriber, Failure == S.Failure, Output == S.Input {
+
+    func receive<S>(subscriber: S) where S: Subscriber, Failure == S.Failure, Output == S.Input {
         currentState.receive(subscriber: subscriber)
     }
 }

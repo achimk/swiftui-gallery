4213ad8c6390d351a484ad36bdec9f58d60ea337
Joachim Kret <joachim.kret@external.toyotaconnected.eu>
2023-08-01 22:49:19 +0200

    Add PollingProcess.

---
+++ CommonUI/Sources/Utilities/PollingProcess.swift
@@ -0,0 +1,111 @@
+import Foundation
+
+public enum PollingState {
+    case idle
+    case running
+    case cancelled
+}
+
+public typealias PollingCompletion = () -> Void
+public typealias PollingInvalidate = () -> Void
+public typealias PollingScheduler = (TimeInterval, @escaping () -> PollingInvalidate) -> PollingInvalidate
+
+public final class PollingProcess {
+    private let timeInterval: TimeInterval
+    private let pollingScheduler: PollingScheduler
+    private let perform: (@escaping PollingCompletion) -> PollingInvalidate
+    private var invalidate: PollingInvalidate?
+
+    @Published
+    public private(set) var state: PollingState = .idle
+
+    public init(
+        timeInterval: TimeInterval,
+        perform: @escaping (@escaping PollingCompletion) -> PollingInvalidate,
+        pollingScheduler: @escaping PollingScheduler = PollingProcess.defaultScheduler
+    ) {
+        self.timeInterval = timeInterval
+        self.perform = perform
+        self.pollingScheduler = pollingScheduler
+    }
+
+    deinit {
+        invalidatePolling()
+    }
+
+    public func start() {
+        if state != .running {
+            state = .running
+            schedulePolling()
+        }
+    }
+
+    public func cancel() {
+        if state == .running {
+            state = .cancelled
+            invalidatePolling()
+        }
+    }
+
+    private func schedulePolling() {
+        guard state == .running else {
+            return
+        }
+
+        let completion: PollingCompletion = { [weak self] in
+            self?.schedulePolling()
+        }
+
+        invalidate = pollingScheduler(timeInterval) { [perform] in
+            perform(completion)
+        }
+    }
+
+    private func invalidatePolling() {
+        invalidate?()
+        invalidate = nil
+    }
+}
+
+extension PollingProcess {
+    private class PollingTimer {
+        private let timeInterval: TimeInterval
+        private let callback: () -> PollingInvalidate
+        private var timer: Timer?
+        private var cancellation: PollingInvalidate?
+
+        init(
+            timeInterval: TimeInterval,
+            callback: @escaping () -> PollingInvalidate
+        ) {
+            self.timeInterval = timeInterval
+            self.callback = callback
+        }
+
+        func schedule() {
+            timer = Timer.scheduledTimer(
+                withTimeInterval: timeInterval,
+                repeats: false,
+                block: { [weak self] _ in
+                    self?.perform()
+                }
+            )
+        }
+
+        func perform() {
+            cancellation = callback()
+        }
+
+        func invalidate() {
+            cancellation?()
+            timer?.invalidate()
+            timer = nil
+        }
+    }
+
+    public static let defaultScheduler: PollingScheduler = { timeInterval, callback in
+        let timer = PollingTimer(timeInterval: timeInterval, callback: callback)
+        timer.schedule()
+        return timer.invalidate
+    }
+}
---
+++ CommonUI/Tests/Utilities/PollingProcessTests.swift
@@ -0,0 +1,216 @@
+import CommonUI
+import XCTest
+
+class PollingProcessTests: XCTestCase {
+    func test_initialise_shouldBeIdle() {
+        let components = makeTestComponents()
+        XCTAssertEqual(components.process.state, .idle)
+    }
+
+    func test_startPolling_shouldUpdateToRunning() {
+        let components = makeTestComponents()
+        components.process.start()
+        XCTAssertEqual(components.process.state, .running)
+    }
+
+    func test_cancelStartedPolling_shouldUpdateToCancelled() {
+        let components = makeTestComponents()
+        components.process.start()
+        components.process.cancel()
+        XCTAssertEqual(components.process.state, .cancelled)
+    }
+
+    func test_cancelDuringExecution_shouldUpdateToCancelled() {
+        let components = makeTestComponents()
+        let process = components.process
+        components.callbacks.onPerform = {
+            process.cancel()
+        }
+
+        components.process.start()
+        components.scheduler.fire()
+
+        XCTAssertEqual(components.process.state, .cancelled)
+    }
+
+    func test_cancelDuringExection_shouldInvokeSchedulerCancellation() {
+        let components = makeTestComponents()
+        let process = components.process
+        components.callbacks.onPerform = {
+            process.cancel()
+        }
+
+        components.process.start()
+        let record = components.scheduler.fire()
+
+        XCTAssertTrue(record?.isCancelled ?? false)
+    }
+
+    func test_polling_shouldReturnCorrectState() {
+        let components = makeTestComponents()
+        var counter = 0
+        components.callbacks.onPerform = {
+            counter += 1
+        }
+
+        components.process.start()
+        components.scheduler.fire()
+        components.scheduler.fire()
+        components.scheduler.fire()
+
+        XCTAssertEqual(components.process.state, .running)
+    }
+
+    func test_polling_shouldInvokeCorrectCount() {
+        let components = makeTestComponents()
+        var counter = 0
+        components.callbacks.onPerform = {
+            counter += 1
+        }
+
+        components.process.start()
+        components.scheduler.fire()
+        components.scheduler.fire()
+        components.scheduler.fire()
+
+        XCTAssertEqual(counter, 3)
+    }
+
+    func test_pollingAndCancel_shouldReturnCorrectState() {
+        let components = makeTestComponents()
+        var counter = 0
+        components.callbacks.onPerform = {
+            counter += 1
+        }
+
+        components.process.start()
+        components.scheduler.fire()
+        components.scheduler.fire()
+        components.scheduler.fire()
+        components.process.cancel()
+
+        XCTAssertEqual(components.process.state, .cancelled)
+    }
+
+    func test_pollingAndCancel_shouldInvokeCorrectCount() {
+        let components = makeTestComponents()
+        var counter = 0
+        components.callbacks.onPerform = {
+            counter += 1
+        }
+
+        components.process.start()
+        components.scheduler.fire()
+        components.scheduler.fire()
+        components.scheduler.fire()
+        components.process.cancel()
+
+        XCTAssertEqual(counter, 3)
+    }
+}
+
+extension PollingProcessTests {
+    private struct TestComponents {
+        let process: PollingProcess
+        let scheduler: TestPollingScheduler
+        let callbacks: PollingCallbacks
+    }
+
+    private func makeTestComponents(
+        timeInterval: TimeInterval = 1
+    ) -> TestComponents {
+        let scheduler = TestPollingScheduler()
+        let callbacks = PollingCallbacks()
+        let process = PollingProcess(
+            timeInterval: timeInterval,
+            perform: callbacks.perform,
+            pollingScheduler: scheduler.schedule(with:callback:)
+        )
+
+        return TestComponents(
+            process: process,
+            scheduler: scheduler,
+            callbacks: callbacks
+        )
+    }
+}
+
+// MARK: - Helpers
+
+private class TestPollingScheduler {
+    private(set) var records: [PollingRecord] = []
+
+    func schedule(
+        with timeInterval: TimeInterval,
+        callback: @escaping () -> PollingInvalidate
+    ) -> PollingInvalidate {
+        let record = PollingRecord(
+            timeInterval: timeInterval,
+            callback: callback
+        )
+        records.append(record)
+        return record.cancel
+    }
+
+    @discardableResult
+    func fire() -> PollingRecord? {
+        guard let record = records.first else {
+            return nil
+        }
+        records.remove(at: 0)
+        record.run()
+        return record
+    }
+}
+
+private class PollingRecord {
+    let timeInterval: TimeInterval
+    private let callback: () -> PollingInvalidate
+    private(set) var isCancelled: Bool = false
+    private var cancellable: PollingInvalidate?
+
+    init(
+        timeInterval: TimeInterval,
+        callback: @escaping () -> PollingInvalidate
+    ) {
+        self.timeInterval = timeInterval
+        self.callback = callback
+    }
+
+    func run() {
+        cancellable = callback()
+    }
+
+    func cancel() {
+        cancellable?()
+        cancellable = nil
+        isCancelled = true
+    }
+}
+
+private class PollingCallbacks {
+    var onBeforePerform: (() -> Void)?
+    var onPerform: (() -> Void)?
+    var onAfterPerform: (() -> Void)?
+    var onInvalidate: (() -> Void)?
+    var shouldAutoComplete = true
+    private var completion: PollingCompletion?
+
+    func perform(_ completion: @escaping PollingCompletion) -> PollingInvalidate {
+        self.completion = completion
+        onBeforePerform?()
+        onPerform?()
+        onAfterPerform?()
+        if shouldAutoComplete {
+            complete()
+        }
+        return { [onInvalidate] in
+            onInvalidate?()
+        }
+    }
+
+    func complete() {
+        completion?()
+        completion = nil
+    }
+}

e6327afa6c59c2893e0a2e1eb09e6ce08eb23675
Joachim Kret <joachim.kret@external.toyotaconnected.eu>
2023-12-05 07:42:39 +0100

    Update calendar component.

--- UIGallery/UIGallery/SwiftUI/DatePicker/DatePickerSampleView.swift
+++ UIGallery/UIGallery/SwiftUI/DatePicker/DatePickerSampleView.swift
@@ -1,4 +1,42 @@
 import SwiftUI
+import UIKit
+
+struct UICalendarViewAdapter: UIViewRepresentable {
+    @Binding var selection: Date
+    var range: ClosedRange<Date>?
+
+    func makeUIView(context: Context) -> UIDatePicker {
+        let datePicker = UIDatePicker(frame: .zero)
+        datePicker.datePickerMode = .date
+        datePicker.preferredDatePickerStyle = .inline
+        datePicker.addTarget(context.coordinator, action: #selector(Coordinator.didUpdateDate(for:)), for: .valueChanged)
+        return datePicker
+    }
+
+    func updateUIView(_ uiView: UIDatePicker, context _: Context) {
+        uiView.date = selection
+        uiView.minimumDate = range?.lowerBound
+        uiView.maximumDate = range?.upperBound
+    }
+
+    func makeCoordinator() -> Coordinator {
+        Coordinator(selection: $selection)
+    }
+
+    final class Coordinator: NSObject {
+        @Binding var selection: Date
+
+        init(selection: Binding<Date>) {
+            _selection = selection
+            super.init()
+        }
+
+        @objc
+        func didUpdateDate(for datePicker: UIDatePicker) {
+            selection = datePicker.date
+        }
+    }
+}
 
 struct DatePickerSampleView: View {
     let dateFormatter: DateFormatter = {
@@ -20,16 +58,13 @@ struct DatePickerSampleView: View {
     var body: some View {
         ScrollView {
             VStack {
-                DatePicker("Date picker:", selection: $date, in: range, displayedComponents: .date)
-                    .labelsHidden()
-
-                DatePicker("Date picker:", selection: $date, in: range, displayedComponents: .date)
-
+                Text("SwiftUI:")
                 DatePicker("", selection: $date, in: range, displayedComponents: .date)
                     .datePickerStyle(.graphical)
 
-                DatePicker("", selection: $date, in: range, displayedComponents: .hourAndMinute)
-                    .datePickerStyle(.wheel)
+                Text("UIKit:")
+                UICalendarViewAdapter(selection: $date, range: range)
+                    .scaledToFit()
 
                 Text("Date is \(date, formatter: dateFormatter)")
             }
